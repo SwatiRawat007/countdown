552f59503547990b742f10b5da60ae868e2cfa6c
Swatirawat007 <swatirawat839@gmail.com>
2024-05-01 19:04:59 -0700

    count

---
+++ count.js
@@ -0,0 +1,21 @@
+const endDate="2 May 2024 5:00 PM"
+document.querySelector("#end-date").innerText=endDate;
+const inputs=document.querySelectorAll("input")
+
+function clock(){
+    const end= new Date(endDate);
+    const now= new Date();
+    const diff= (end-now)/1000;
+    console.log(diff);
+
+    inputs[0].value=Math.floor(diff/(60*60)/24);
+    inputs[1].value= Math.floor((diff/60*60)%24);
+    inputs[2].value= Math.floor(diff/60)%60;
+    inputs[3].value=Math.floor(diff)%60;
+
+}
+clock();
+
+setInterval(()=>{
+    clock()
+},1000)
